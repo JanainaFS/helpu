219591ff30755fd117ab3d48bb24cbdcc8e55569
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2019-06-16 16:38:32 -0300

    Add rota de exibir Chamada

--- routes/colaborador.js
+++ routes/colaborador.js
@@ -7,7 +7,7 @@ const ColabController = require("../controllers/ColabController");
 
 router.get("/indexC", ColabController.index);
 router.post("/cadastrar/add", ColabController.cadastrarColab);
-//rota de exibir chamada cadastrada
+router.get("/chamada", ColabController.chamada);
 router.get("/chamada/add", ColabController.addChamada);
 router.post("/chamada/nova", ColabController.novaChamada);
 router.get("/chamada/deletar/:id", ColabController.deletar);
