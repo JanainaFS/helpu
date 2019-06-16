a5a824e5b25be951f69a92c0781c4fd853f1ed2e
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2019-06-16 12:17:39 -0300

    Controller index colaborador

--- controllers/ColabController.js
+++ controllers/ColabController.js
@@ -3,5 +3,8 @@ require("../models/Usuario");
 const Usuario = mongoose.model("usuarios");
 
 module.exports = {
+  index(req, res) {
+    return res.render("colaborador/index");
+  },
   cadastrarColab(req, res) {}
 };
