252b1731ed23444eb87313095fea353270fb6745
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2019-06-16 12:16:15 -0300

    Rota index colaborador

--- routes/colaborador.js
+++ routes/colaborador.js
@@ -5,6 +5,7 @@ const router = express.Router();
 //Controller Colab
 const AdminController = require("../controllers/ColabController");
 
+router.get("/indexC", AdminController.index);
 router.post("/cadastrar/add", AdminController.cadastrarColab);
 
 module.exports = router;
