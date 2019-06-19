b783da0be56cf73aea070afdcc8f19741b370019
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2019-06-19 19:38:02 -0300

    Crud User Admin/Suporte

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,21 @@
 //Rotas de Administrador
 const express = require("express");
 const router = express.Router();
+const { eAdmin } = require("../helpers/eAdmin");
 
 //Controller Admin
 const AdminController = require("../controllers/AdminController");
 
-router.get("/", AdminController.index);
+//Rotas de Setor
 router.get("/setor", AdminController.setor);
 router.get("/setor/add", AdminController.addSetor);
 router.post("/setor/novo", AdminController.novoSetor);
 router.get("/setor/deletar/:id", AdminController.deletar);
+
+//Rotas de Usuário Suporte
+router.get("/suporte", AdminController.userSuporte);
+router.post("/cadastrarSup/add", AdminController.CadUserSuporte);
+router.get("/suporte/:id", AdminController.verSuporte);
+router.get("/suporte/deletar/:id", AdminController.deletarSuporte);
 
 module.exports = router;
