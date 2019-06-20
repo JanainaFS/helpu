86d54300c82984d88ce9b4f85afb556813829b3e
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2019-06-20 18:18:12 -0300

    Rotas colab

--- routes/colaborador.js
+++ routes/colaborador.js
@@ -1,23 +1,24 @@
 //Rotas de Colaborador
 const express = require("express");
 const router = express.Router();
+const { eColab } = require("../helpers/eColab");
 
 //Controller Colab
 const ColabController = require("../controllers/ColabController");
 
 //Rota de Index do Usuário Colaborador
-router.get("/indexC", ColabController.index);
+router.get("/indexC", eColab, ColabController.index);
 
 //Rotas de Colaborador
-router.post("/cadastrar/add", ColabController.cadastrarColab);
+router.post("/cadastrar/add", eColab, ColabController.cadastrarColab);
 
 //Rotas de Chamadas
-router.get("/chamada/add", ColabController.addChamada);
-router.get("/chamada/:id", ColabController.verChamada);
-router.post("/chamada/nova", ColabController.novaChamada);
-router.get("/chamada/deletar/:id", ColabController.deletar);
+router.get("/chamada/add", eColab, ColabController.addChamada);
+router.get("/chamada/:id", eColab, ColabController.verChamada);
+router.post("/chamada/nova", eColab, ColabController.novaChamada);
+router.get("/chamada/deletar/:id", eColab, ColabController.deletar);
 
 //Rota de deletar usuário para teste
-router.get("/deletarUser/:id", ColabController.deletarUser);
+router.get("/deletarUser/:id", eColab, ColabController.deletarUser);
 
 module.exports = router;
