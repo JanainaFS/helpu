eb296f0faf4fdd07a63f933d977f90045ec39c52
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2019-06-20 18:21:19 -0300

    Rotas supote

--- routes/suporte.js
+++ routes/suporte.js
@@ -1,14 +1,15 @@
 //Rotas de Suporte
 const express = require("express");
 const router = express.Router();
+const { eSup } = require("../helpers/eSup");
 
 //Controller Suporte
 const SuporteController = require("../controllers/SuporteController");
 
 //Rotas de Chamadas / Suporte
-router.get("/chamadas", SuporteController.chamadas);
-router.get("/chamadas/:id", SuporteController.verChamada);
-router.post("/chamadas/finalizar/:id", SuporteController.finalizar);
+router.get("/chamadas", eSup, SuporteController.chamadas);
+router.get("/chamadas/:id", eSup, SuporteController.verChamada);
+router.post("/chamadas/finalizar/:id", eSup, SuporteController.finalizar);
 //router.post("/chamadas/editar/:id", SuporteController.editarChamada);
 
 module.exports = router;
